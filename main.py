97463aac62823463af596e24efe8cf209d98ce33
Rexari <79110842+Rexari@users.noreply.github.com>
2023-09-25 13:40:00 +0600

    Commit 1

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print('Hello world')
+print('Input start program phraze: ')
+a = input()
+if (a == 'hello world') or (a == 'Hello world') or (a == 'Hello World'):
+    print('Correct')
